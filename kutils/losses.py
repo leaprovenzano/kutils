bfd8424d799634b23beeebe85e95e8e0dbea4887
leaprovenzano <leaprovenzano@gmail.com>
2017-07-27 10:35:46 +0100

    added bce_dice_loss

--- kutils/losses.py
+++ kutils/losses.py
@@ -11,3 +11,7 @@ def soft_f2_loss(y_true, y_pred):
 
 def dice_coef_loss(y_true, y_pred):
     return -metrics.dice_coef(y_true, y_pred)
+
+def bce_dice_loss(y_true, y_pred):
+    """this was written by keras user bguberfain see: https://www.kaggle.com/bguberfain/naive-keras"""
+    return binary_crossentropy(y_true, y_pred) - metrics.dice_coef(y_true, y_pred)
